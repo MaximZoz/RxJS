e4c19df044cfa4a889eebc1d695e33753c01867b
MaximZoz <max591018@gmail.com>
2020-10-05 20:52:55 +0200

    2. Оптимизация стримов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,18 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  sub: Subscription;
+  sub: Subscription; /* сюда мы складываем результаты работы метода subscribe */
 
   constructor() {
     const intervalStream$ = interval(1000);
     // intervalStream$ - переменная с реактивностью (стримом от RxJS)
     this.sub = intervalStream$.subscribe((value) => {
+      /* сдесь мы подписываемся */
       console.log(value); /* 1.2.3.4.5.6 каждую сек новое значение */
     });
   }
 
   stop() {
-    this.sub.unsubscribe();
+    this.sub.unsubscribe(); /* сдесь мы отписываемся */
   }
 }
