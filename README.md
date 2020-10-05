7f435bc95fc19b112e36fed00c5e9bd4851ad32e
MaximZoz <max591018@gmail.com>
2020-10-05 22:58:17 +0200

    4. Создание своего стрима

--- README.md
+++ README.md
@@ -4,6 +4,35 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## RxJS
 
-операторы в RxJS
+Создание своего стрима
 
-1. вызываем метод .pipe() в который помещаем оператор filter() , map() и switchMap()
+1. вызываем функции setTimeout в которые помещаем функции метод next, error, complete. Вызываем метод subscribe и эти функции
+
+/\*  
+git init (инициализируем git)
+git config --global user.name "MaximZoz"
+git config --global user.email max591018@gmail.com
+git config --list
+
+git status (узнаём что лежит в проекте)
+git add . (добавляем в гит все файлы)
+git commit -a -m"3. Как использовать операторы"
+git push -u origin master (пушим коммит на GitHub)
+
+git remote add origin https://github.com/MaximZoz/test1.git
+git log - просмотр коммитов
+git reset --hard HEAD~1 удаляем последний коммит
+git reset --hard f794efbc6d20319d948b5628f77f285718c5bdd1 -удаляем все коммиты до этого по коду
+git push --force -сохраняем
+
+git checkout -- index.html возвращаем файл к коммиту
+git checkout . возвращаем все файлы к коммиту
+git clone https://github.com/MaximZoz/test1.git скачиваем данные из коммита
+git fetch забираем новые данные
+git pull новые данные появляются в проекте
+
+git branch ветка1 команда создаёт ветку
+git branch -v показать какие есть ветки в проекте
+git checkout ветка1 переключиться в ветку1
+git checkout -b ветка2 создаёт и переключает в ветку2
+\*/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { map, filter, switchMap } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,24 +10,26 @@ export class AppComponent {
   sub: Subscription; /* сюда мы складываем результаты работы метода subscribe */
 
   constructor() {
-    const intervalStream$ = interval(1000);
-    // intervalStream$ - переменная с реактивностью (стримом от RxJS)
-    this.sub = intervalStream$
-      .pipe(
-        switchMap((value) => interval(500)),
-        filter(
-          (value) => value % 2 === 0
-        ) /* отдам тольк оте значения, которые кратно делятся на 2 */,
-        map(
-          (value) => `mapped value ${value}`
-        ) /* на каждой итерации мы преобразовываем строчку и добавляем значение mapped value*/
-      ) /* метод позволяет применять некоторое кол-во операторов для данного стрима */
-      .subscribe((value) => {
-        /* сдесь мы подписываемся */
-        console.log(value); /* 1.2.3.4.5.6 каждую сек новое значение */
-      });
+    const stream$ = new Observable((observer) => {
+      setTimeout(() => {
+        observer.next(1);
+      }, 1500);
+      setTimeout(() => {
+        observer.complete();
+      }, 2100);
+      setTimeout(() => {
+        observer.error('Что-то не так');
+      }, 2000);
+      setTimeout(() => {
+        observer.next(2);
+      }, 2500);
+    });
+    this.sub = stream$.subscribe(
+      (value) => console.log('next:', value),
+      (error) => console.log('error:', error),
+      () => console.log('complete  ')
+    );
   }
-
   stop() {
     this.sub.unsubscribe(); /* сдесь мы отписываемся */
   }
